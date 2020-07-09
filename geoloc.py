35311e5b4f0bfe6530f12e46ca20c698c96b1fed
shibaji7 <shibaji7@vt.edu>
2020-07-09 12:30:54 -0400

    Azimuth angle of ray bearing

--- geoloc.py
+++ geoloc.py
@@ -20,7 +20,7 @@ import os
 
 from davitpy.pydarn.radar import radar
 from davitpy.pydarn.radar.radFov import fov
-
+import davitpy.utils as utils
 
 def geolocate_radar_fov(rad):
     """ Geolocate each range cell """
@@ -28,10 +28,13 @@ def geolocate_radar_fov(rad):
     s = r.sites[0]
     f = fov(site=s)
     blen, glen = len(f.beams), len(f.gates)
-    glat, glon = np.zeros((blen, glen)), np.zeros((blen, glen))
+    glat, glon, azm = np.zeros((blen, glen)), np.zeros((blen, glen)), np.zeros((blen, glen))
     for i,b in enumerate(f.beams):
         for j,g in enumerate(f.gates):
             glat[i,j], glon[i,j] = f.latCenter[b,g], f.lonCenter[b,g]
+            d = utils.geoPack.calcDistPnt(f.latFull[b,g], f.lonFull[b,g], 300,
+                    distLat=f.latFull[b, g + 1], distLon=f.lonFull[b, g + 1], distAlt=300)
+            azm[i,j] = d["az"]
     fname = "data/sim/{rad}.geolocate.data.nc".format(rad=rad)
     rootgrp = Dataset(fname, "w", format="NETCDF4")
     rootgrp.description = """ Fitacf++ : Geolocated points for each range cells. """
@@ -44,7 +47,14 @@ def geolocate_radar_fov(rad):
     beam[:], gate[:], = f.beams, f.gates
     _glat = rootgrp.createVariable("lat","f4",("nbeam","ngate"))
     _glon = rootgrp.createVariable("lon","f4",("nbeam","ngate"))
-    _glat[:], _glon[:] = glat, glon
+    _azm = rootgrp.createVariable("azm","f4",("nbeam","ngate"))
+    _glat[:], _glon[:], _azm[:] = glat, glon, azm
+    _glat.description = "Geographic latitude of range cell"
+    _glon.description = "Geographic longitude of range cell"
+    _azm.description = "Azimuthal angle ray vector bearing"
+    _glat.units = "Deg(o)"
+    _glon.units = "Deg(o)"
+    _azm.units = "Deg(o)"
     rootgrp.close()
     os.system("gzip "+fname)
     return
